80e688c34d89154ccdcd4335d17faf7dad825934
RonanSilasSmith <smith.ronan@outlook.com>
2020-12-06 17:21:26 -0900

    simplifies service worker for testing

--- service-worker.js
+++ service-worker.js
@@ -4,9 +4,6 @@ const CACHE_NAME = APP_PREFIX + VERSION;
 
 const FILES_TO_CACHE = [
     "./public/index.html",
-    "./public/js/idb.js",
-    "./public/js/index.js",
-    "./public/css/style.css"
 ];
 
 self.addEventListener('install', function (e) {
